3f4393d1aee618fd269d24e49f48e1330f232eb2
watchhill101 <1443256811@qq.com>
2025-09-08 12:07:06 +0800

    feat: 测试分支

--- shortPlayApp/config/index.js
+++ shortPlayApp/config/index.js
@@ -26,7 +26,7 @@ const config = {
   api: {
     // 开发环境
     development: {
-      baseURL: `http://192.168.0.223:3000/api`,
+      baseURL: `http://192.168.0.155:3000/api`,
       timeout: 10000,
     },
     // 测试环境
